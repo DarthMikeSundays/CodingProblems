d927d7c4ca2c5b726f04bc1b4f7fcf40f0e3f8ec
ProMikeCoder2020 <59772138+ProMikeCoder2020@users.noreply.github.com>
2020-11-22 15:39:39 +0000

    Update AlphabetSoup.py

--- AlphabetSoup.py
+++ AlphabetSoup.py
@@ -1,3 +1,8 @@
+# Problem description:
+# Taken a string the program orders it's chars by alphabetic order
+# The output has to be in lower char format(so this program is not case sensitive)
+# The spaces of the input should be ignored
+
 string=input("Chose a string.")
 string=string.upper()
 alphabet=["A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","S","T","U","V","W","X","Y","Z"]
